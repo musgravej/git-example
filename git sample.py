3470cc14831c65397538ff4f1223e31d07db421a
jim <jim.musgrave@gmail.com>
2018-08-29 20:48:55 -0500

    break up print commands

--- git sample.py
+++ git sample.py
@@ -2,6 +2,11 @@ import os
 
 
 def main():
+    # print("one print to rule them all!!!")
+    # How about three different print messages?
+
+    print("one print")
+    print("one print to rule")
     print("one print to rule them all!!!")
 
 
